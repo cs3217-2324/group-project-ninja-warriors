98dd16e4b9d3f76764998c035f0189f5c2faad44
Joshen Lim <e0525998@u.nus.edu>
2024-04-06 15:03:05 +0800

    refactor(realtime, slashaoe): set slash to player position, remove collider

--- NinjaWarriors/Primitives/Skills/SlashAOESkill.swift
+++ NinjaWarriors/Primitives/Skills/SlashAOESkill.swift
@@ -32,7 +32,25 @@ class SlashAOESkill: EntitySpawnerSkill {
     func spawnEntity(from casterEntity: Entity, in manager: EntityComponentManager) -> Entity {
         print("[SlashAOESkill] Activated by \(casterEntity)")
         let slashAOE = SlashAOE(id: RandomNonce().randomNonceString(), casterEntity: casterEntity)
-        manager.add(entity: slashAOE, isAdded: false)
+        
+    
+        guard let playerRigidbody = manager.getComponent(ofType: Rigidbody.self, for: casterEntity) else {
+            print("[SlashAOESkill] No player rigidbody found")
+            return slashAOE
+        }
+        
+        let shape = Shape(center: playerRigidbody.position, halfLength: Constants.defaultSize)
+        
+        let rigidbody = Rigidbody(id: RandomNonce().randomNonceString(), entity: slashAOE,
+                                  angularDrag: 0.0, angularVelocity: 0.0, mass: 8.0,
+                                  rotation: 0.0, totalForce: Vector.zero, inertia: 0.0,
+                                  position: shape.center, velocity: Vector.zero)
+
+        let spriteComponent = Sprite(id: RandomNonce().randomNonceString(),
+                                     entity: slashAOE, image: "slash-effect", width: 100,
+                                     height: 100, health: 10, maxHealth: 100)
+        
+        manager.add(entity: slashAOE, components: [rigidbody, spriteComponent], isAdded: false)
         return slashAOE
     }
     
