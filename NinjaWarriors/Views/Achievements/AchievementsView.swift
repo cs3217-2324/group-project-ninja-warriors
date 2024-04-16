648d8f99af84e2959add80302aef630498e8a0a4
jivesh <jiveshm2000@gmail.com>
2024-04-16 17:21:55 +0800

    Show achievements in a view

---
+++ NinjaWarriors/Views/Achievements/AchievementsView.swift
@@ -0,0 +1,34 @@
+//
+//  AchievementsView.swift
+//  NinjaWarriors
+//
+//  Created by Jivesh Mohan on 16/4/24.
+//
+
+import SwiftUI
+
+struct AchievementsView: View {
+    @ObservedObject var achievementManager: AchievementManager
+    let columns = [GridItem(.flexible())]
+
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    let sortedAchievements = achievementManager.achievements.sorted {
+                        $0.isUnlocked && !$1.isUnlocked
+                    }
+                    ForEach(achievementManager.achievements, id: \.title) { achievement in
+                        AchievementRow(achievement: achievement)
+                    }
+                }
+            }
+        }.toolbar {
+            Text("Achievements")
+        }
+    }
+}
+
+#Preview {
+    AchievementsView(achievementManager: AchievementManager(userID: "guestID", metricsSubject: MetricsRepository()))
+}
