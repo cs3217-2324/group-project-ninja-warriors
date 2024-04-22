a6d2953662ed265fbcc8fd5adf0e72988c84987f
ARPspoofing <mohdreyaaz99@gmail.com>
2024-04-23 00:39:04 +0800

    Add final collection system

--- NinjaWarriors/ECS/Systems/CollectionSystem/CollectionSystem.swift
+++ NinjaWarriors/ECS/Systems/CollectionSystem/CollectionSystem.swift
@@ -17,11 +17,12 @@ class CollectionSystem: System {
     func update(after time: TimeInterval) {
         let collectors = manager.getAllComponents(ofType: Collector.self)
         for collector in collectors {
+            collector.updateTime(time)
             if let collider = manager.getComponent(ofType: Collider.self, for: collector.entity),
                let collidedEntityID = collider.collidedEntities.first,
                let collidedEntity = manager.entity(with: collidedEntityID),
                let collectable = manager.getComponent(ofType: Collectable.self, for: collidedEntity) {
-                collector.addItem(of: collectable.entityType, count: 1)
+                collector.addItem(of: collectable.entityType, count: 1, currEntity: collidedEntity.id)
                 manager.add(entity: collectable.entity, components: [DestroyTag(id: RandomNonce().randomNonceString(), entity: collectable.entity)])
             }
         }
